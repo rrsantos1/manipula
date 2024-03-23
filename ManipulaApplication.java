4bca574ad1259f8e36e1beb3d0bf77c3d5f55ce5
rrsantos1 <164591700+rrsantos1@users.noreply.github.com>
2024-03-22 23:32:53 -0300

    Add files via upload

---
+++ ManipulaApplication.java
@@ -0,0 +1,13 @@
+package com.example.manipula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManipulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManipulaApplication.class, args);
+	}
+
+}
